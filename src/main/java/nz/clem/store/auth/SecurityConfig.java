a45d0996038589d6adafe26d24e6837e4a6fdbbc
Clem White <clemw@hotmail.com>
2025-06-29 20:29:26 +1200

    Update SecurityConfig to allow public access to products APIs

--- src/main/java/nz/clem/store/auth/SecurityConfig.java
+++ src/main/java/nz/clem/store/auth/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig {
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(c -> c
                 .requestMatchers("/carts/**").permitAll()
+                    .requestMatchers("/products/**").permitAll()
                     .requestMatchers("/admin/**").hasRole(Role.ADMIN.name())
                 .requestMatchers(HttpMethod.POST,"/users").permitAll()
                 .requestMatchers(HttpMethod.POST,"/auth/login").permitAll()
